4d89bdf3f9515fc06d17595a62a65f3bee7df025
Yurgish <humenyuriipetrovych@gmail.com>
2025-08-18 18:26:56 +0300

    feat: add CI workflow and environment configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [react()],
+  },
+  plugins: [react()],
+  base: "/WorldDailyNews",
 });
